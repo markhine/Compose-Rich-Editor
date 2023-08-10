dcd061654d5230937f4234d6cc08dfdfd4498f93
Mark Hine <mark@juliusspencer.co.nz>
2023-08-10 16:21:29 +1200

    Revert "Fix jitpack build error"
    This reverts commit 73fa8aeac6d53407404b45369f7ab268d867a545.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,6 @@ plugins {
 //    alias(libs.plugins.dokka).apply(false)
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-}
-
 allprojects {
     repositories {
         google()
